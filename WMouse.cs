4a3f374a14e7f948a2690e119a0301e75c3df34f
Jerdik <maratf76@gmail.com>
2025-02-10 01:59:52 +0500

    2025 02 10 0159

---
+++ WMouse.cs
@@ -0,0 +1,16 @@
+﻿//using System.Drawing;
+
+namespace ConsoleApp2
+{
+    public class WMouse
+    {
+        public float mouseX;
+        public float mouseY;
+
+        public WMouse()
+        {
+            mouseX = 0;
+            mouseY = 0;
+        }
+    }
+}
